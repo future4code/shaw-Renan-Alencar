fd039daaae838aba327292bc8966c7fab9727a40
Renan Martinez <remam_martinez89@hotmail.com>
2022-03-07 11:55:54 -0300

    exercício 2 concluído

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -10,8 +10,9 @@ function retornaTamanhoArray(array) {
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  
+  return array.reverse();
 }
+
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
