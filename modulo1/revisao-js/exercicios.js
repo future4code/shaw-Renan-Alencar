ac8e0c0339099bda2f78c630ca4cea5090320907
Renan Martinez <remam_martinez89@hotmail.com>
2022-03-07 14:37:13 -0300

    exercício 7 concluído

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -1,11 +1,10 @@
 // ATENÇÃO!!!
-//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!!
 //    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
-
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-   return array.length
+  return array.length;
 }
 
 // EXERCÍCIO 02
@@ -13,10 +12,9 @@ function retornaArrayInvertido(array) {
   return array.reverse();
 }
 
-
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  function comparar(a , b) {
+  function comparar(a, b) {
     return a - b;
   }
   return array.sort(comparar);
@@ -25,27 +23,27 @@ function retornaArrayOrdenado(array) {
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
   const pares = array.filter((num) => {
-   return num % 2 === 0
-  })
+    return num % 2 === 0;
+  });
   return pares;
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
   const pares = array.filter((num) => {
-    return num % 2 === 0
-   })
-   return pares.map((num) => {
-     return num * num;
-   });
+    return num % 2 === 0;
+  });
+  return pares.map((num) => {
+    return num * num;
+  });
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
   let maior = 0;
-  for(let num of array){
-    if(num > maior) {
-      maior = num
+  for (let num of array) {
+    if (num > maior) {
+      maior = num;
     }
   }
   return maior;
@@ -53,55 +51,51 @@ function retornaMaiorNumero(array) {
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
+  let maiorNumero = 0;
+  let menorNumero = 0;
+  if (num1 > num2) {
+    maiorNumero = num1;
+    menorNumero = num2;
+  } else {
+    maiorNumero = num2;
+    menorNumero = num1;
+  }
+  let maiorDivisivelPorMenor = maiorNumero % menorNumero === 0;
+  let diferenca = maiorNumero - menorNumero;
 
+  return {
+    maiorNumero: maiorNumero,
+    maiorDivisivelPorMenor: maiorDivisivelPorMenor,
+    diferenca: diferenca,
+  };
 }
 
 // EXERCÍCIO 08
-function retornaNPrimeirosPares(n) {
-   
-}
+function retornaNPrimeirosPares(n) {}
 
 // EXERCÍCIO 09
-function classificaTriangulo(ladoA, ladoB, ladoC) {
-
-}
+function classificaTriangulo(ladoA, ladoB, ladoC) {}
 
 // EXERCÍCIO 10
-function retornaSegundoMaiorESegundoMenor(array) {
-  
-}
+function retornaSegundoMaiorESegundoMenor(array) {}
 
 // EXERCÍCIO 11
-function retornaChamadaDeFilme(filme) {
-   
-}
+function retornaChamadaDeFilme(filme) {}
 
 // EXERCÍCIO 12
-function retornaPessoaAnonimizada(pessoa) {
-   
-}
+function retornaPessoaAnonimizada(pessoa) {}
 
 // EXERCÍCIO 13A
-function retornaPessoasAutorizadas(pessoas) {
-   
-}
+function retornaPessoasAutorizadas(pessoas) {}
 
 // EXERCÍCIO 13B
-function retornaPessoasNaoAutorizadas(pessoas) {
-  
-}
+function retornaPessoasNaoAutorizadas(pessoas) {}
 
 // EXERCÍCIO 14
-function retornaContasComSaldoAtualizado(contas) {
-
-}
+function retornaContasComSaldoAtualizado(contas) {}
 
 // EXERCÍCIO 15A
-function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
-}
+function retornaArrayOrdenadoAlfabeticamente(consultas) {}
 
 // EXERCÍCIO 15B
-function retornaArrayOrdenadoPorData(consultas) {
-   
-}
+function retornaArrayOrdenadoPorData(consultas) {}
