329a93d4a68604c68e9f3eba3faba1074861547a
Renan Martinez <remam_martinez89@hotmail.com>
2022-03-23 21:03:30 -0300

    efetuado exercícios insta e desafio 1

---
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post
+          nomeUsuario={'renan'}
+          fotoUsuario={'https://picsum.photos/50/50?a=2'}
+          fotoPost={'https://picsum.photos/200/150?a=2'}
+        />
+
+        <Post
+          nomeUsuario={'jõao'}
+          fotoUsuario={'https://picsum.photos/50/50?a=3'}
+          fotoPost={'https://picsum.photos/200/150?a=3'}
+        />
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
---
+++ modulo2/estados-insta4/insta4/src/components/SecaoCompartilhar/SecaoCompartilhar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import styled from "styled-components"
+
+class SecaoCompartilhar extends React.Component {
+  render(){
+    return
+    <div>
+
+    </div>
+  }
+}
+
+export default SecaoCompartilhar;
