305bc1c3d83384d147134a6558fd6ee5f2d05fc8
Renan Martinez <remam_martinez89@hotmail.com>
2022-05-01 13:49:47 -0300

    estilizado pagina aplicar formulario

--- modulo3/projeto-labex/labex/src/pages/ApplicationFormPage/ApplicationFormPage.js
+++ modulo3/projeto-labex/labex/src/pages/ApplicationFormPage/ApplicationFormPage.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { goBack, goToLoignPage, goHome } from "../../routes/coordinator";
-import { MainContainer } from "./styled";
+import { MainForm, ButtonDiv } from "./styled";
 import { useParams } from "react-router-dom";
 import { useRequestData } from "../../hooks/useRequestData";
 import { BASE_URL } from "../../constants/Base_url";
+import { PAISES } from "../../constants/Paises";
+
 import { useForm } from "../../hooks/useForm";
 import axios from "axios";
 import { Header } from "../Header/Header";
+
 
 
 const ApplicationFormPage = () => {
@@ -21,7 +24,7 @@ const ApplicationFormPage = () => {
     profession: "",
     country: "",
   });
-
+   
   //verifica se a getTrips não está vazio e faz um filter pelo ID da viagem e depois um map retornando apenas o nome da viagem.
   const tripSelected =
     getTrips.trips &&
@@ -45,66 +48,76 @@ const ApplicationFormPage = () => {
     })
   };
 
+  //faz um map no array de paises recebido de constants
+  const listPais = PAISES.map((pais) => {
+    return <option key={pais.ordem} value={pais.nome}>{pais.nome}</option>
+  })
+ 
   return (
-    <MainContainer>
-      <Header 
-      first={{function: goHome, text: "Inicio",}}
-      second={{function: goToLoignPage, text: "Login",}}
+    <div>
+      <Header
+        first={{ function: goHome, text: "Inicio" }}
+        second={{ function: goToLoignPage, text: "Login" }}
       />
-      ApplicationFormPage
-      <button onClick={() => goBack(navigate)}>Voltar</button>
-      <div>
+      <MainForm>
         <h1>Faça sua inscrição</h1>
-        <h2>Viagem escolhida: {tripSelected}</h2>
-        <form onSubmit={enviar}>
-          <input
-            value={form.name}
-            name="name"
-            onChange={handleChange}
-            placeholder={"Nome"}
-            required
-            pattern={"^.{3,}"}
-            title={"O nome deve ter no mínimo 3 letras"}
-          />
-          <input
-            value={form.age}
-            name="age"
-            onChange={handleChange}
-            placeholder={"Idade"}
-            required
-            type="number"
-            min={18}
-          />
-          <input
-            value={form.profession}
-            name="profession"
-            onChange={handleChange}
-            placeholder={"Profissão"}
-            required
-            pattern={"^.{10,}"}
-            title={"A profissão deve ter no mínimo  10 caracteres"}
-          />
-          <input
-            value={form.country}
-            name="country"
-            onChange={handleChange}
-            placeholder={"País"}
-            required
-          />
-          <input
-            value={form.applicationText}
-            name="applicationText"
-            onChange={handleChange}
-            placeholder={"Texto Candidatura"}
-            required
-            pattern={"^.{30,}"}
-            title={"A candidatura deve ter no mínimo  30 caracteres"}
-          />
-          <button>Enviar</button>
-          <button onClick={() => goBack(navigate)}>Voltar</button>
-        </form>
-      </div>
-    </MainContainer>
+        <div>
+          <form onSubmit={enviar}>
+            <h2>Viagem escolhida: {tripSelected}</h2>
+            <input
+              value={form.name}
+              name="name"
+              onChange={handleChange}
+              placeholder={"Nome"}
+              required
+              pattern={"^.{3,}"}
+              title={"O nome deve ter no mínimo 3 letras"}
+            />
+            <input
+              value={form.age}
+              name="age"
+              onChange={handleChange}
+              placeholder={"Idade"}
+              required
+              type="number"
+              min={18}
+            />
+            <input
+              value={form.profession}
+              name="profession"
+              onChange={handleChange}
+              placeholder={"Profissão"}
+              required
+              pattern={"^.{10,}"}
+              title={"A profissão deve ter no mínimo  10 caracteres"}
+            />
+            <select
+              value={form.country}
+              name="country"
+              onChange={handleChange}
+              placeholder={"País"}
+              required
+            >
+              <option value="">Seleciona um país</option>
+              {listPais}
+            </select>
+            <input
+              value={form.applicationText}
+              name="applicationText"
+              onChange={handleChange}
+              placeholder={"Texto Candidatura"}
+              required
+              pattern={"^.{30,}"}
+              title={"A candidatura deve ter no mínimo  30 caracteres"}
+            />
+            <ButtonDiv>
+              <button>Enviar</button>
+              <button onClick={() => goBack(navigate)}>Voltar</button>
+            </ButtonDiv>
+          </form>
+        </div>
+      </MainForm>
+    </div>
   );
 };
 
