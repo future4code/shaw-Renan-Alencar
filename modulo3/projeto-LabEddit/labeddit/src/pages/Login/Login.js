7480ff5d3cd1c71aa440074b1b176246ee5e9382
Renan Martinez <remam_martinez89@hotmail.com>
2022-05-10 22:55:48 -0300

    estilizado pagina login

--- modulo3/projeto-LabEddit/labeddit/src/pages/Login/Login.js
+++ modulo3/projeto-LabEddit/labeddit/src/pages/Login/Login.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import fotoLogo from '../../assets/fotoLogo.JPG'
+import { MainContainer, Logo, H1, P, Form, ButtonContinuar, ButtonConta } from './styled'
+import TextField from '@mui/material/TextField';
+import { useForm } from '../../hooks/useForm';
 
 const Login = () => {
+  const {form, onChange, clean} = useForm({email: '', password: ''})
+
+  const onSubmitForm = () => {
+
+  }
+
   return (
-    <div>Login</div>
-  )
+    <MainContainer>
+      <Logo src={fotoLogo} alt="logo labeddit" />
+      <H1>LabEddit</H1>
+      <P>Um projeto de rede social</P>
+
+      <Form onSubmit={onSubmitForm}>
+        <TextField
+          label={"Nome"}
+          name={"email"}
+          value={form.email}
+          onChange={onChange}
+          margin={"dense"}
+          fullWidth
+        />
+        <TextField
+          label={"Senha"}
+          name={"password"}
+          value={form.password}
+          onChange={onChange}
+          margin={"dense"}
+          fullWidth
+        />
+      </Form>
+
+      <ButtonContinuar>Continuar</ButtonContinuar>
+      <ButtonConta>Criar Conta</ButtonConta>
+
+    </MainContainer>
+  );
 }
 
 export default Login
---
+++ modulo3/projeto-LabEddit/labeddit/src/pages/Login/styled.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* margin: 0px; */
+  margin-top: 89px;
+` 
+export const Logo = styled.img`
+  width: 116px;
+`
+
+export const H1 = styled.h1`
+  font-size: 36px;
+  font-weight: bolder;
+  color: #373737;
+  width: 152px;
+  line-height: 47px;
+  margin: 0px;
+`
+
+export const P = styled.p`
+  font-size: 16px;
+  line-height: 20.8px;
+  margin: 0px;
+`
+
+export const Form = styled.form`
+  margin: 100px 33px 0 33px;
+`
+export const ButtonContinuar = styled.button`
+  background: linear-gradient(90deg, #ff6489 0%, #f9b24e 100%);
+  border-radius: 27px;
+  width: 325px;
+  height: 51px;
+  margin-top: 56px;
+  font-size: 18px;
+  font-weight: bolder;
+  color: white;
+`;
+
+export const ButtonConta = styled.button`
+  background: white;
+  border-radius: 27px;
+  width: 325px;
+  height: 51px;
+  margin-top: 50px;
+  font-size: 18px;
+  font-weight: bolder;
+  color: #FE7E02;
+`;
