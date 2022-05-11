72fd4a31075be4ea5677197dcef20ccef5cdc34b
Renan Martinez <remam_martinez89@hotmail.com>
2022-05-11 10:59:16 -0300

    estilizado tela detalhes

--- modulo3/projeto-LabEddit/labeddit/src/pages/DetalhesPost/DetalhesPost.js
+++ modulo3/projeto-LabEddit/labeddit/src/pages/DetalhesPost/DetalhesPost.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
+import {MainContainer, Comentario, Button} from './styled'
 
 const DetalhesPost = () => {
   return (
-    <div>DetalhesPost</div>
+    <div>
+      <Header />
+      <MainContainer>
+      <p>Components post...</p>
+      <Comentario type="text" placeholder='Adiconar um comentário'/>
+      <Button>Responder</Button>
+      <p>Components respostas..</p>
+
+      </MainContainer>
+    </div>
   )
 }
 
