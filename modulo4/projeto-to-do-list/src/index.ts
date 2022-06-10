054b097c2c59f6c5044cf91050822c9a7403ecdd
Renan Martinez <remam_martinez89@hotmail.com>
2022-06-10 18:15:23 -0300

    efetuado exercicio 4

--- modulo4/projeto-to-do-list/src/index.ts
+++ modulo4/projeto-to-do-list/src/index.ts
@@ -1,10 +1,12 @@
 import app from "./app";
+import createTask from "./endpoints/createTask";
 import createUser from "./endpoints/createUser";
 import editUser from "./endpoints/editUser";
 import getUser from "./endpoints/getUser";
 
 app.post("/user", createUser);
 
+app.post("/task", createTask);
 
 app.get('/user/:id', getUser);
 
