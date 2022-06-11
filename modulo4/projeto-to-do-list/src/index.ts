bb57f8fb89d035d5b85aa48f180f1452bf8355e4
Renan Martinez <remam_martinez89@hotmail.com>
2022-06-11 12:00:18 -0300

    efetuado desadio 6

--- modulo4/projeto-to-do-list/src/index.ts
+++ modulo4/projeto-to-do-list/src/index.ts
@@ -2,12 +2,15 @@ import app from "./app";
 import createTask from "./endpoints/createTask";
 import createUser from "./endpoints/createUser";
 import editUser from "./endpoints/editUser";
+import getAllUsers from "./endpoints/getAllUsers";
 import getTask from "./endpoints/getTask";
 import getUser from "./endpoints/getUser";
 
 app.post("/user", createUser);
 
 app.post("/task", createTask);
+
+app.get('/user/all', getAllUsers);
 
 app.get('/user/:id', getUser);
 
