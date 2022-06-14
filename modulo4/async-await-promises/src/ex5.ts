43b8caeefb6a2883513f92979f3877c79f2d4c26
Renan Martinez <remam_martinez89@hotmail.com>
2022-06-13 23:19:07 -0300

    efetuado exercicio 5

---
+++ modulo4/async-await-promises/src/ex5.ts
@@ -0,0 +1,37 @@
+import axios from "axios"
+import { baseURL } from "./baseURL"
+
+const message =  "Curso back Labenu";
+
+const  getsubscribers =  async () => {
+  const response = await 
+    axios.get(`${baseURL}/subscribers`) 
+    return response.data
+}
+
+const getIds = (subscribers: any) => {
+  return subscribers.map((subscriber: any) => {
+    return subscriber.id;
+  });
+};
+
+const sendNotification = async (ids: string[]): Promise<void> => {
+  for (const id of ids) {
+    try {
+      await axios
+        .post(`${baseURL}/notifications`, {
+          subscriberId: id,
+          message: message,
+        })
+        .then(() => {
+          console.log(`Notificação enviada a ${id}`);
+        });
+    } catch (error) {
+      console.log(`Erro ao enviar para ${id}`);
+    }
+  }
+};
+
+getsubscribers()
+  .then(getIds)
+  .then(sendNotification)
