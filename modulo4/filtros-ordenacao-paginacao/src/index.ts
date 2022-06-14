022f5918bb5ab0e3b06e714da6e1bbc67cc591d2
Renan Martinez <remam_martinez89@hotmail.com>
2022-06-14 19:08:18 -0300

    efetuado exercicio 4

--- modulo4/filtros-ordenacao-paginacao/src/index.ts
+++ modulo4/filtros-ordenacao-paginacao/src/index.ts
@@ -1,6 +1,7 @@
 import { app } from "./app";
 import { exercicio2 } from "./endpoints/exer2";
 import { exercicio3 } from "./endpoints/exer3";
+import { exercicio4 } from "./endpoints/exer4";
 import { getAllUsers } from "./endpoints/getAllUsers";
 import { getPathParamsTipo } from "./endpoints/getPathParamsTipo";
 import { getQueryParamsNome } from "./endpoints/getQueryParamsNome";
@@ -10,5 +11,7 @@ app.get("/users?", getQueryParamsNome);
 app.get("/users/:tipo", getPathParamsTipo);
 app.get("/exer2?", exercicio2);
 app.get("/exer3?", exercicio3);
+app.get("/exer4?", exercicio4);
+
 
 
