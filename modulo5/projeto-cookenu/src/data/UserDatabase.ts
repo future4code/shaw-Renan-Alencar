9b8e7fd1892a73b7124151905d45dc0412388b54
Renan Martinez <remam_martinez89@hotmail.com>
2022-06-29 16:49:47 -0300

    criado endpoint getProfile

--- modulo5/projeto-cookenu/src/data/UserDatabase.ts
+++ modulo5/projeto-cookenu/src/data/UserDatabase.ts
@@ -13,4 +13,11 @@ export class UserDatabase extends BaseDatabase {
       .where({ email });
     return result;
   };
+
+  public getById = async ( id: string) => {
+    const [result] = await this.getConnection()("User_Cookenu")
+      .select("*")
+      .where( {id} );
+    return result;
+  };
 }
--- modulo5/projeto-cookenu/src/index.ts
+++ modulo5/projeto-cookenu/src/index.ts
@@ -1,8 +1,12 @@
 import app from "./app";
 import { createUser } from "./endpoints/createUser";
+import { getProfile } from "./endpoints/getProfile";
 import { login } from "./endpoints/login";
 
 app.post("/signup", createUser)
 app.post("/login", login);
+
+app.get("/user/profile", getProfile);
+
 
 
