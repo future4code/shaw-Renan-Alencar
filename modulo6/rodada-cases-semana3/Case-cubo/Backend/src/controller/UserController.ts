99e0747920cbaadb20c091f7da26d7bad71d42a2
Renan Martinez <remam_martinez89@hotmail.com>
2022-08-01 16:04:03 -0300

    implementado userController

--- modulo6/rodada-cases-semana3/Case-cubo/Backend/src/controller/UserController.ts
+++ modulo6/rodada-cases-semana3/Case-cubo/Backend/src/controller/UserController.ts
@@ -1,5 +1,36 @@
+import { Request, Response } from "express";
+import { UserBusiness } from "../business/UserBusiness";
+import { BaseDatabase } from "../data/BaseDatabase";
+import { User } from "../model/User";
+import { CreatePostInputDTO } from "../types/createPostInputDTO";
+
 export class UserController {
-  createUser = async (params:any) => {
-    
+  constructor(
+    private userBusiness: UserBusiness
+  ){}
+
+  createUser = async (req: Request, res: Response) => {
+    try {
+
+      const {first_name, last_name, participation } = req.body
+
+      const input: CreatePostInputDTO = {
+        first_name,
+        last_name,
+        participation,
+      }
+
+      await this.userBusiness.createUser(input)
+
+      res.status(200).send({ message: "User registered successfully"});
+      
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).send(error.message)
+    }
+    res.status(500).send("Erro no signup")
+    } finally{
+      BaseDatabase.destroyConnection()
+    }
   }
 }
---
+++ modulo6/rodada-cases-semana3/Case-cubo/Backend/src/types/createPostInputDTO.ts
@@ -0,0 +1,5 @@
+export interface CreatePostInputDTO {
+  first_name: string;
+  last_name: string;
+  participation: number;
+}
